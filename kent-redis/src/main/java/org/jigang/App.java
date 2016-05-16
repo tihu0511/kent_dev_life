1cbdf908df1deff69af8d806acad8322f63c90e3
BF100271 <huozhanbai@baofoo.com>
2016-05-16 13:34:50 +0800

    spring-redis-data demo

---
+++ kent-redis/src/main/java/org/jigang/App.java
@@ -0,0 +1,13 @@
+package org.jigang;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
